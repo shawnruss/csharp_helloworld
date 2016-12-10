5acae088fd79aea3ecc1bc256f2d4a140bb05d3d
Shawn Russell <srussell@randstad.com.sg>
2016-12-10 22:06:46 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Karin!");
+            Console.Beep(300, 2000);
+
+            
+        }
+    }
+}
